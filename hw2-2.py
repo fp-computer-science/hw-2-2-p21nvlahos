eb919631b5cd0eacb047158cdf73373586cc0a96
Niko <p21nvlahos@fairfieldprep.org>
2020-09-19 16:42:16 -0400

    Add hw edit

--- hw2-2.py
+++ hw2-2.py
@@ -1,8 +1,12 @@
 # Author: NV 9/18/2020
 
-fthrow = 1*4
-binside = 2*7
-boutside = 3*3
+fthrow = 1
+binside = 2
+boutside = 3
 
-total = fthrow + binside + boutside
+free_throws = 4
+two_point_shot = 7
+three_point_shot = 3
+
+total = (fthrow * free_throws)+ (binside * two_point_shot) + (boutside * three_point_shot)
 print("The player scored", total, "points")
